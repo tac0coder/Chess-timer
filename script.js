1b186280e578c252a637b56c661658d5aa8efe25
uglyfence <tacojulian@outlook.com>
2020-12-19 15:34:36 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+var timep1=[prompt('How many hours per player?'),prompt('How many minutes per player?'),prompt('How many seconds per player?'),999]
+var timep2=timep1
+var timing = 'p1'
+var p1 = [document.getElementById('p1name'),document.getElementById('p1'),document.getElementById('p1ms')]
+var p2 = [document.getElementById('p2name'),document.getElementById('p2'),document.getElementById('p2ms')]
+var decreaseS = function(){
+  if(timing == 'p1'){
+    timep1[2]-=1
+    p1[1].innerHTML = timep1[0].toString()+':'+timep1[1]+':'+timep1[2]
+  }else{
+    timep2[2]-=1
+    p2[1].innerHTML = timep2[0].toString()+':'+timep2[1]+':'+timep2[2]
+  }
+}
+var decreaseM = function(){
+  if(timing == 'p1'){
+    timep1[1]-=1
+    p1[1].innerHTML = timep1[0].toString()+':'+timep1[1]+':'+timep1[2]
+  }else{
+    timep2[1]-=1
+    p2[1].innerHTML = timep2[0].toString()+':'+timep2[1]+':'+timep2[2]
+  }
+}
+var decreaseH = function(){
+  if(timing == 'p1'){
+    timep1[0]-=1
+    p1[1].innerHTML = timep1[0].toString()+':'+timep1[1]+':'+timep1[2]
+  }else{
+    timep2[0]-=1
+    p2[1].innerHTML = timep2[0].toString()+':'+timep2[1]+':'+timep2[2]
+  }
+}
